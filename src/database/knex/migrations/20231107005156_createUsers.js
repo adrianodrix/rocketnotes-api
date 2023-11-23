f255b1c679b8ff03d0792cbc42329791daa2a860
Adriano Santos <adrianodrix@gmail.com>
2023-11-23 00:33:06 -0300

    fix: add table users migrate

---
+++ src/database/knex/migrations/20231107005156_createUsers.js
@@ -0,0 +1,11 @@
+export const up = knex => knex.schema.createTable('users', table => {
+    table.increments('id')
+    table.string('name').notNullable()
+    table.string('email').notNullable().unique()
+    table.string('password').notNullable()
+    table.string('avatar')
+    table.timestamp('created_at').default(knex.fn.now())
+    table.timestamp('updated_at').default(knex.fn.now())
+})
+
+export const down = knex => knex.schema.dropTableIfExists('users')
